4cdb9608b5fe29372b0d598717ef32662ef05327
Aidan Fitzpatrick <aidan@reincubate.com>
2022-12-11 12:15:05 +0000

    Handled debounce.

--- keyboards/gmmk/pro/rev1/iso/keymaps/afit/config.h
+++ keyboards/gmmk/pro/rev1/iso/keymaps/afit/config.h
@@ -16,6 +16,9 @@
 
 #pragma once
 
+#undef DEBOUNCE
+#define DEBOUNCE 8
+
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
     #define RGB_DISABLE_WHEN_USB_SUSPENDED  true
